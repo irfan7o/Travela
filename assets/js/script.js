96a5f66c65cfcf26070ac0b9a3492db56249fdb5
Irfan <kartolo727@gmail.com>
2024-07-05 19:44:45 +0800

    Travel Website

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,23 @@ const preloader = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", () => {
     preloader.classList.add("remove");
-})
+});
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
